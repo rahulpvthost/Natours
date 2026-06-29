855479984c12570c762a22bfdb68fceddb7eb1ec
rahulpvthost <rahulpvthost@gmail.com>
2026-06-29 13:45:08 +0530

    create new folder and file tourModels and add mongoose

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,18 +1,5 @@
-const fs = require('fs');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
-);
-
-exports.checkID = (req, res, next, val) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
+const Tour= require('./../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -29,47 +16,39 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 exports.getTour = (req, res) => {
   console.log(req.params);
 
   const id = req.params.id * 1; 
-  const tour = tours.find((el) => el.id === id); 
+  // const tour = tours.find((el) => el.id === id); 
 
-  res.status(200).json({
-    status: 'success',
+  // res.status(200).json({
+  //   status: 'success',
 
-    data: {
-      tour,
-    },
-  });
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 exports.createTour = (req, res) => {
  
-
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
       res.status(201).json({
         status: 'success',
-        data: {
-          tour: newTour,
+        // data: {
+        //   tour: newTour,
 
-        },
+        // },
       });
     },
-  );
+  
  
-};
+
 exports.updateTour = (req, res) => {
   res.status(200).json({
     status: 'success',
