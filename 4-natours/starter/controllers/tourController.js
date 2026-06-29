e3b9fd303de787b1222159ff05c9810b8bba6348
rahulpvthost <rahulpvthost@gmail.com>
2026-06-29 15:06:18 +0530

    creater  create tour to add data in database

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,17 +1,9 @@
 const Tour= require('../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
+
 
 exports.getAllTours = (req, res) => {
-  //console.log(req.requestTime);
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
@@ -35,15 +27,28 @@ exports.getTour = (req, res) => {
   //   },
   // });
 };
-exports.createTour = (req, res) => {
- 
+exports.createTour = async (req, res) => {
+  try{
+     const newTour = await Tour.create(req.body);
+
       res.status(201).json({
         status: 'success',
-        // data: {
-        //   tour: newTour,
+        data: {
+          tour: newTour,
 
-        // },
+        },
       });
+
+  }catch(err){
+    res.status(400).json({
+      status:'fail',
+      message:'Invalid data sent'
+    })
+  }
+//  const newTours =new Tour({})
+//  newTours.save()
+
+    
     };
   
  
