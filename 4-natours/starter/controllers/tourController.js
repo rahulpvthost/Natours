599c88261a866c93f7da9b1c4d73934b964dda79
rahulpvthost <rahulpvthost@gmail.com>
2026-07-02 22:54:24 +0530

    add query to our project by which we select tour by property

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,9 +2,27 @@ const Tour= require('../models/tourModel');
 
 exports.getAllTours =  async (req, res) => {
   try{
+    //Buid query
+    const queryObj =  {...req.query};
+    const excludeFields =['page','sort','limit','fields'];
+    excludeFields.forEach(el => delete queryObj[el]);
+     
 
-   const tours = await Tour.find();
+  //  const tours = await Tour.find({
+  //   duration:5,
+  //   difficulty:'easy'
+  //  });
+  // const tours = await Tour.find()
+  //         .where('duration')
+  //         .equals(5)
+  //         .where('difficulty')
+  //         .equals('easy');
 
+  const query = Tour.find(queryObj);
+  
+//Excute query
+   const tours = await query;
+      
   res.status(200).json({
     status: 'success',
     results: tours.length,
