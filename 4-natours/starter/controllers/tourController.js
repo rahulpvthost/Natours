1b7d71a1555c2687549125325b43cee150c6fd59
rahulpvthost <rahulpvthost@gmail.com>
2026-07-03 11:30:07 +0530

    sorting in database

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,26 +1,29 @@
+
 const Tour= require('../models/tourModel');
 
 exports.getAllTours =  async (req, res) => {
   try{
     //Buid query
-    //1.Filtering 
+    //1A.Filtering 
     const queryObj =  {...req.query};
     const excludeFields =['page','sort','limit','fields'];
     excludeFields.forEach(el => delete queryObj[el]);
-     //2.Advance Filtering
+     //1B.Advance Filtering
   let querystr = JSON.stringify(queryObj);
   querystr = querystr.replace(/\b(gte|gt|lte|lt)\b/g,match => `$${match}`);
-  console.log(JSON.parse(querystr));
-
-  const query = Tour.find(JSON.parse(querystr));
-
-
-
-       //{difficulty:'easy',duration:{$gte:5}}
-       //{difficulty:'easy',duration:{gte:5}}
-       //gte,gt,lte,lt
   
 
+  let query = Tour.find(JSON.parse(querystr));
+    //2.Sorting
+       if(req.query.sort){
+        const sortBy = req.query.sort.split(',').join(' ');
+        query =query.sort(sortBy);
+       }else{
+        query = query.sort('-createdAt');
+       } 
+        //sort('price ratingsAverage') in this it sort by price   and if same price then sort by ratingsAverage
+        //write in postman as ?sort=price,ratingsAverage
+       
 //Excute query
    const tours = await query;
       
