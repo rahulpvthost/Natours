17f14d2b15782aa5a1a83c8b76f0fb96c298b989
rahulpvthost <rahulpvthost@gmail.com>
2026-07-05 22:50:12 +0530

    know about virtual property

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -53,6 +53,15 @@ const tourSchema = new mongoose.Schema({
   },
   startDates: [Date]   
 
+},{
+  toJSON:{virtuals:true},
+  toObject:{virtuals:true},
 });
+
+tourSchema.virtual('durationWeeks').get(function(){
+  return this.duration/7;
+})
+
+
 const Tour = mongoose.model('Tour',tourSchema);
 module.exports =Tour;
