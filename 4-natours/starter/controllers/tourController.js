eebdfe1a3c325a12582b1b36f5dd0296454d3cc9
rahulpvthost <rahulpvthost@gmail.com>
2026-07-05 21:25:15 +0530

    doing Aggregation pipeline :Matching and Grouping

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -117,4 +117,41 @@ exports.deleteTour = async  (req, res) => {
 }
 };
 
+exports.getTourStats = async (req,res) =>{
+  try{
+    const stats = await Tour.aggregate([
+      {
+        $match: {ratingAverage:{$gte:4.5}}
+      },
+      {
+        $group:{
+          _id:{$toUpper:'$difficulty'},
+          numTours:{$sum:1},
+          numRatings:{$sum:'$ratingQuantity'},
+          avgRating:{$avg:'$ratingAverage'},
+          avgPrice:{$avg:'$price'},
+          minPrice:{$min:'$price'},
+          maxPrice:{$max:'$price'}
+       
+        }
+      },
+       {
+        $sort:{avgPrice:1} 
+        }//,{
+      //   $match:{_id:{$ne:'EASY'}}
+      //  } 
+    ]);
+    res.status(200).json({
+      status:'success',
+      data:{
+        stats
+      }
+    });
 
+  }catch(err){
+    res.status(404).json({
+      status:'fail',
+      message:err
+    });
+  }
+}
