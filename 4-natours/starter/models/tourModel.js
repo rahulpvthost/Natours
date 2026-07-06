1af205533573aca64bc8793d828482fffd9a1305
rahulpvthost <rahulpvthost@gmail.com>
2026-07-06 12:01:47 +0530

    Aggregation middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -103,7 +103,14 @@ next();
 
 tourSchema.post(/^find/,function(docs,next){
   console.log(`Query took ${Date.now()-this.start}milliseconds`)
-  console.log(docs);
+ 
+  next();
+})
+
+//AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate',function(next){
+  this.pipeline().unshift({$match:{secretTour : {$ne:true}}});
+ // console.log(this.pipeline());
   next();
 })
 
