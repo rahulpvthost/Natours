8dead66e9ad92171ca302ee1807514c01e7c0018
rahulpvthost <rahulpvthost@gmail.com>
2026-07-06 11:17:10 +0530

    query middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
   name:{
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema({
     unique:true,
     trim:true,
   },
+  slug:String,
   duration: {
     type:Number,
     required: [true,'A tour must have a duration']  
@@ -51,8 +53,11 @@ const tourSchema = new mongoose.Schema({
     default:Date.now(),
     select:false  
   },
-  startDates: [Date]   
-
+  startDates: [Date],   
+   secretTour:{
+    type:Boolean,
+    default:false
+   }
 },{
   toJSON:{virtuals:true},
   toObject:{virtuals:true},
@@ -61,7 +66,46 @@ const tourSchema = new mongoose.Schema({
 tourSchema.virtual('durationWeeks').get(function(){
   return this.duration/7;
 })
+//The main reason to use a virtual 
+// property is to avoid storing data that can be calculated from other data.
 
+
+
+//Document middleware:runs before save() and.create()
+tourSchema.pre('save',function(next){
+  this.slug = slugify(this.name,{lower:true});
+next();
+});
+
+// tourSchema.pre('save',function(next){
+//   console.log('will save document');
+//   next();
+// });
+
+// tourSchema.post('save',function(doc,next){
+// console.log(doc);
+// next();
+// });
+
+//Query middleware
+
+// this middleware hide data in postman which have secretTour :true,
+// if we use find method it didnt show or in database we can see the secret tour
+
+
+// tourSchema.pre('find',function(next){
+tourSchema.pre(/^find/,function(next){
+  this.find({secretTour:{$ne:true}});
+
+  this.start = Date.now();
+next();
+});
+
+tourSchema.post(/^find/,function(docs,next){
+  console.log(`Query took ${Date.now()-this.start}milliseconds`)
+  console.log(docs);
+  next();
+})
 
 const Tour = mongoose.model('Tour',tourSchema);
 module.exports =Tour;
