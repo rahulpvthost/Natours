46c591b7328c5a00ecb641b3178c4294dedc2a14
rahulpvthost <rahulpvthost@gmail.com>
2026-07-08 23:28:11 +0530

    create separate file for error handling

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,7 +1,8 @@
 
 const express = require('express');
 const morgan = require('morgan');
-
+const AppError = require('./utilis/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./Routes/tourRoutes');
 const userRouter = require('./Routes/userRoutes');
 
@@ -31,22 +32,16 @@ app.all('*', (req, res, next) => {
   //   status: 'fail',
   //   message: `Can't find ${req.originalUrl} on this server!`
   // })
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.statusCode = 404;
-  err.status = 'fail';
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.statusCode = 404;
+  // err.status = 'fail';
 
-  next(err);
+  // next(err);
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 //Error handling middleware
-app.use((err, req, res, next) => {
-err.statusCode = err.statusCode || 500;
-err.status = err.status || 'error';
-
-res.status(err.statusCode).json({
-  status: err.status,
-  message: err.message  
- });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
