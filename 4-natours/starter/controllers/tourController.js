b626e5cedef766caaa5a7996e8ec2317851b51f8
rahulpvthost <rahulpvthost@gmail.com>
2026-07-09 13:38:45 +0530

    if we get or delete element by id then in id last  one element i interchange it show status succes that error to to handle thats why if use if() condition

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,6 +2,7 @@
 const Tour= require('../models/tourModel');
 const APIFeatures = require('../utilis/apiFeatures');
 const catchAsync = require('../utilis/catchasync');
+const AppError = require('../utilis/appError');
 
 exports.aliasTopTours = (req,res,next) =>{
   req.query.limit = '5';
@@ -32,6 +33,10 @@ exports.getAllTours = catchAsync(async (req, res,next) => {
 exports.getTour = catchAsync(async (req, res,next) => {
 
  const tour =  await Tour.findById(req.params.id);
+
+if(!tour){
+return next(new AppError('No tour found with that ID',404));
+}
  res.status(200).json({
     status: 'success',
 
@@ -62,6 +67,13 @@ exports.updateTour = catchAsync(async (req, res,next) => {
       new:true,
       runValidators:true
     });
+
+    
+   if(!tour){
+    return next(new AppError('No tour found with that ID',404));
+  }
+
+
      res.status(200).json({
     status: 'success',
     data: {
@@ -74,7 +86,16 @@ exports.updateTour = catchAsync(async (req, res,next) => {
 
 exports.deleteTour = catchAsync(async  (req, res,next) => {
   
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  
+if(!tour){
+return next(new AppError('No tour found with that ID',404));
+}
+
+
+
+
   res.status(204).json({
     status: 'success',
     data: null,
--- 4-natours/starter/utilis/catchasync.js
+++ 4-natours/starter/utilis/catchasync.js
@@ -1,5 +1,5 @@
 module.exports = fn => (req,res,next )=>{
-        fn(req,res,next).catch(next);
+       Promise.resolve(fn(req,res,next)).catch(next);
     };
 
 
