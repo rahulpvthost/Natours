2383e28af396089cb6bf1634645c0e4b794f8056
rahulpvthost <rahulpvthost@gmail.com>
2026-07-09 13:59:38 +0530

    wrap error in funtion for the production and development

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,9 +1,40 @@
-module.exports =(err, req, res, next) => {
+ 
+  const sendErrorDev = (err,res) =>{
+          
+  res.status(err.statusCode).json({
+   status: err.status,
+   message: err.message ,
+   stack: err.stack,
+   error: err
+ });
+  }
+
+  const sendErrorProd = (err,res) =>{
+    if(err.isOperational){
+  res.status(err.statusCode).json({
+  status: err.status,
+  message: err.message ,
+  })
+}else{
+  //1) Log error
+ // 2) Send generic message
+  console.error('ERROR 💥', err);
+  res.status(500).json({
+  status: 'error',
+  message: 'Something went very wrong!'
+  });
+}
+  };
+
+ 
+ 
+ module.exports =(err, req, res, next) => {
 err.statusCode = err.statusCode || 500;
 err.status = err.status || 'error';
 
-res.status(err.statusCode).json({
-  status: err.status,
-  message: err.message  
- });
-}
+ if(process.env.NODE_ENV === 'development'){
+ sendErrorDev(err, res);
+ }else if(process.env.NODE_ENV === 'production'){              
+  sendErrorProd(err, res);
+ }
+};
