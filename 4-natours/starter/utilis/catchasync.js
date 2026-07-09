a26e98f55ca4e00605165bcdf83ac8dd0e83b856
rahulpvthost <rahulpvthost@gmail.com>
2026-07-09 13:11:56 +0530

    add comment in catchasync.js file about working

--- 4-natours/starter/utilis/catchasync.js
+++ 4-natours/starter/utilis/catchasync.js
@@ -1,3 +1,9 @@
 module.exports = fn => (req,res,next )=>{
         fn(req,res,next).catch(next);
     };
+
+
+//     When a request arrives Express calls the wrapper. 
+// The wrapper calls fn(req,res,next) which returns a Promise because fn is async.
+// If the Promise rejects, .catch(next) runs and calls next(err).
+//  That triggers your global error-handling middleware.
