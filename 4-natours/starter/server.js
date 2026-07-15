38820036474c70213333c6c62e7ef320163e14ee
rahulpvthost <rahulpvthost@gmail.com>
2026-07-15 21:38:21 +0530

    add error handler for the database information error happen like password wrong

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -17,9 +17,17 @@ mongoose
     useFindAndModify:false
 }).then(() =>{
  console.log("DB connection successfull");
-})
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+process.on('unhandledRejection',err=>{
+  console.log(err.name,err.message);
+  console.log('UNHANDLER REJECTION! SHUTTING DOWN...');
+  server.close(()=>{
+  process.exit(1);
+  })
+})
