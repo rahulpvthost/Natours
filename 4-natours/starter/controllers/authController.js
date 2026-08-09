5ad49b84956f1d5d248ff678c7f786338b67b318
rahulpvthost <rahulpvthost@gmail.com>
2026-08-09 22:44:13 +0530

    admin and lead-guide can delete only tour data

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -15,7 +15,8 @@ exports.signup =  catchAsync(async (req,res,next)=>{
         name:req.body.name,
         email:req.body.email,
         password:req.body.password,
-        passwordConfirm:req.body.passwordConfirm    
+        passwordConfirm:req.body.passwordConfirm,
+            role: req.body.role    
     })
     const token = signToken(newUser._id);
     
@@ -78,3 +79,10 @@ exports.protect = catchAsync(async(req,res,next)=>{
 
     next();
 });
+exports.restrictTo = (...roles)=>(req,res,next)=>{
+        // roles ['admin','lead-guide']. role='user'
+        if(!roles.includes(req.user.role)){
+            return next(new AppError('You do not have permission to perform this action',403));
+        }
+        next();
+    }
