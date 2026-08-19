245e719c54ff574212324618600181daec03f2d9
rahulpvthost <rahulpvthost@gmail.com>
2026-08-19 22:43:04 +0530

    embedding done of data modeling

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const User =require('./userModel');
 const slugify = require('slugify');
 //const validator = require('validator');
 
@@ -96,7 +97,7 @@ const tourSchema = new mongoose.Schema({
    },
    locations:[
     {
-      type:{
+      type:{ 
         type:String,
         default:'Point',
         enum:['Point']
@@ -106,7 +107,8 @@ const tourSchema = new mongoose.Schema({
       description:String,
       day:Number
     }
-   ]
+   ],
+   guides:Array
   },{
   toJSON:{virtuals:true},
   toObject:{virtuals:true},
@@ -125,6 +127,15 @@ tourSchema.pre('save',function(next){
   this.slug = slugify(this.name,{lower:true});
 next();
 });
+
+
+tourSchema.pre('save', async function(next){
+  const guidesPromises = this.guides.map( async id=>  await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  next();
+});
+
+
 
 // tourSchema.pre('save',function(next){
 //   console.log('will save document');
@@ -149,6 +160,11 @@ tourSchema.pre(/^find/,function(next){
   this.start = Date.now();
 next();
 });
+
+
+
+
+
 
 tourSchema.post(/^find/,function(docs,next){
   console.log(`Query took ${Date.now()-this.start}milliseconds`)
