934f9a3252aad2693a2af6b701e92f0b89471c2b
rahulpvthost <rahulpvthost@gmail.com>
2026-09-04 00:19:52 +0530

    handle get all reviews

--- 4-natours/starter/Routes/reviewRoutes.js
+++ 4-natours/starter/Routes/reviewRoutes.js
@@ -5,6 +5,7 @@ const authController = require('./../controllers/authController');
 const router = express.Router({ mergeParams:true});
 
  //POST /tour/:tourId/reviews
+ //GET /tour/:tourId/reviews
  // POST /reviews
 router.
 route('/')
