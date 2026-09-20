ed879e42ddb6cb1e4044869a271749a79c44f7ee
rahulpvthost <rahulpvthost@gmail.com>
2026-09-21 01:00:24 +0530

    booking all done crud operation

--- 4-natours/starter/controllers/bookingController.js
+++ 4-natours/starter/controllers/bookingController.js
@@ -18,7 +18,7 @@ exports.getCheckoutSession = catchAsync(async(req ,res , next) =>{
         payment_method_types: ['card'],
         success_url:`${req.protocol}://${req.get('host')}/?tour=${
             req.params.tourId
-        }&user=${req.user.id}&price=&{tour.price}`,
+        }&user=${req.user.id}&price=${tour.price}`,
         cancel_url:`${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourId,
@@ -51,8 +51,14 @@ exports.createBookingCheckout =catchAsync(async(req,res,next)=>{
     // this is only Temporary,becase it is unsecure:everyone can make bookings without paying
     const {tour ,user, price}=req.query;
 
-    if(!tour && !user && !price)return next();
+    if(!tour || !user || !price)return next();
 
     await Booking.create({tour,user,price});
     res.redirect(req.originalUrl.split('?')[0]);  
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
