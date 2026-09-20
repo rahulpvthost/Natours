3cc5c99d38b06fc4c0f6952917f4cc2649b763c7
rahulpvthost <rahulpvthost@gmail.com>
2026-09-21 00:33:42 +0530

    done add booking tour in booking section of user

--- 4-natours/starter/models/bookingModel.js
+++ 4-natours/starter/models/bookingModel.js
@@ -25,12 +25,12 @@ const bookingSchema = new mongoose.Schema({
   }
 });
 
-bookingSchema.pre(/^find/, function(next) {
+bookingSchema.pre(/^find/, function() {
   this.populate('user').populate({
     path: 'tour',
     select: 'name'
   });
-  next();
+  
 });
 
 const Booking = mongoose.model('Booking', bookingSchema);
